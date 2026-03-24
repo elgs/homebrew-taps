7ed7972d202ca86e0b2cd718612ff866d038e32a
Qian Chen <elgs1980@hotmail.com>
2026-03-24 12:48:03 -0700

    Update mac-tray-commands to v1.0.2

--- Casks/mac-tray-commands.rb
+++ Casks/mac-tray-commands.rb
@@ -1,6 +1,6 @@
 cask "mac-tray-commands" do
-  version "1.0.1"
-  sha256 "8e1089e6041c6d52bf25d9322197852d3615437dfceb16f62d5beda34fb2ff4a"
+  version "1.0.2"
+  sha256 "e06248786dfa5e2d193ea94cae21adea37a045d4e51e21278c3b86cb9cc1d36b"
 
   url "https://github.com/elgs/mac-tray-commands/releases/download/v#{version}/MacTrayCommands.dmg"
   name "Mac Tray Commands"
