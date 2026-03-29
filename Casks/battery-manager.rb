4b8d9ca6837449beef49b275e72beb4f8e921751
Qian Chen <elgs1980@hotmail.com>
2026-03-29 01:15:58 -0700

    Update battery-manager to v0.0.8

--- Casks/battery-manager.rb
+++ Casks/battery-manager.rb
@@ -1,6 +1,6 @@
 cask "battery-manager" do
-  version "0.0.7"
-  sha256 "932b9a8dac407e8d38c79e6640e0d47e524e53c1bc5d84d1bb71cf896ee2d55d"
+  version "0.0.8"
+  sha256 "8f6d5619e3aa8a6a138b0c22f25ed530e1de1c6c98be54e673b3a4fb1edf6de6"
 
   url "https://github.com/elgs/battery-manager/releases/download/v#{version}/BatteryManager.dmg"
   name "BatteryManager"
