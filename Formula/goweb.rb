94c2a75e9fa94ad4e7dc513966652da6f3fb0c22
Qian Chen <elgs1980@hotmail.com>
2026-08-10 11:49:39 -0700

    release v0.0.13.

--- Formula/goweb.rb
+++ Formula/goweb.rb
@@ -1,7 +1,7 @@
 class Goweb < Formula
   desc "Multi domain/host web server written in Golang"
   homepage "https://github.com/elgs/goweb"
-  url "https://github.com/elgs/goweb.git", tag: "v0.0.12"
+  url "https://github.com/elgs/goweb.git", tag: "v0.0.13"
   license "MIT"
 
   depends_on "go" => :build
